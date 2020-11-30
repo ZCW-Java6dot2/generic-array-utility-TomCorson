91356b6b1b8aaff005c2512ffd1400815daa7bec
TomCorson <corsontom@yahoo.com>
2020-11-30 18:03:26 -0500

    starting point

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,5 +3,24 @@ package com.zipcodewilmington.arrayutility;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility <T>{
+    private T [] arr;
+    public ArrayUtility(T [] inputArray) {
+    }
+
+    public  Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        return 0;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        return  null;
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+        return 0;
+    }
+
+    public T[] removeValue(T valueToRemove) {
+        return null;
+    }
 }
