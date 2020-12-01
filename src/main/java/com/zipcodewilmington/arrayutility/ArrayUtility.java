15c18a7a06debebaf57502ee89f9a74605e3b5da
TomCorson <corsontom@yahoo.com>
2020-11-30 19:44:31 -0500

    doing good

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,19 +1,76 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.*;
+
 /**
  * Created by leon on 3/6/18.
  */
 public class ArrayUtility <T>{
-    private T [] arr;
+    private T [] array;
     public ArrayUtility(T [] inputArray) {
+        this.array = inputArray;
     }
 
     public  Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
-        return 0;
+        int mergedSize = arrayToMerge.length  + array.length;
+        T [] merged = (T[]) new Object [mergedSize];
+        int a = 0;
+
+        for(int j = 0; j < arrayToMerge.length; j++){
+            merged[j] = arrayToMerge[j];
+        }
+
+        for(int i = arrayToMerge.length;i < mergedSize; i++){
+            merged[i] = array[a];
+            a++;
+        }
+
+        Integer count = 0;
+        for(T e: merged){
+            if(valueToEvaluate == e){
+                count++;
+            }
+        }
+        return count;
     }
 
     public T getMostCommonFromMerge(T[] arrayToMerge) {
-        return  null;
+        int mergedSize = arrayToMerge.length  + array.length;
+        T [] merged = (T[]) new Object [mergedSize];
+        int a = 0;
+
+        for(int j = 0; j < arrayToMerge.length; j++){
+            merged[j] = arrayToMerge[j];
+        }
+
+        for(int i = arrayToMerge.length;i < mergedSize; i++){
+            merged[i] = array[a];
+            a++;
+        }
+        Arrays.sort(merged);
+
+        Map<T,Integer> hm = new HashMap<>();
+        for(int e = 0; e < merged.length; e++){
+            T key = merged[e];
+            if(hm.containsKey(key)){
+                int frequency = hm.get(key);
+                frequency++;
+                hm.put(key,frequency);
+            }
+            else {
+                hm.put(key,1);
+            }
+        }
+        int maxCount = 0;
+        T result = null;
+        for(Map.Entry<T,Integer> val: hm.entrySet()){
+            if(maxCount < val.getValue()){
+                result = val.getKey();
+                maxCount = val.getValue();
+            }
+        }
+
+        return  result;
     }
 
     public Integer getNumberOfOccurrences(T valueToEvaluate) {
